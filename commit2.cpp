dd2a32ead925c70c4b4dc30a25623d25f787f01d
Manish Yadav <my3596418@gmail.com>
2026-06-29 22:40:29 +0530

    experiment 2

---
+++ commit2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 3000; i++) {
+        ofstream file("commit.txt", ios::app);
+        file << "Commit " << i << "\n";
+        file.close();
+
+        system("git add .");
+
+        string cmd = "git commit -m \"Commit " + to_string(i) + "\"";
+        system(cmd.c_str());
+    }
+
+    return 0;
+}
